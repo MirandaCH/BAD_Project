4be072b4be1d9c8bfe36d71260f5392cb0199eec
BerinaDgt <berinadurguti@gmail.com>
2015-01-30 17:05:13 +0100

    Calculation changes
    fe

--- PRS/Forms/frmFirstShift.vb
+++ PRS/Forms/frmFirstShift.vb
@@ -10,10 +10,6 @@ Public Class frmFirstShift
         cmd.Connection = con
 
     End Sub
-
-    Private Sub btnBack_Click(sender As Object, e As EventArgs)
-        Close()
-    End Sub
      
     Private Sub ShowTotals()
         cmd.Parameters.Clear()
@@ -39,35 +35,6 @@ Public Class frmFirstShift
         txtTotal.Text = Convert.ToDecimal(txtTb.Text) + Convert.ToDecimal(txtTc.Text) + Convert.ToDecimal(txtTt.Text)
     End Sub
 
-    Private Sub btnBpaid_Click(sender As Object, e As EventArgs)
-        cmd.Parameters.Clear()
-        cmd.CommandText = "INSERT INTO Tickets (RegistrationDate, TicketType, Total, UserID) VALUES (@RD, 2, 110, @UID)"
-        cmd.Parameters.Add(New SqlParameter("@RD", DateTime.Now))
-        cmd.Parameters.Add(New SqlParameter("@UID", Session.UserID))
-
-        con.Open()
-        cmd.ExecuteNonQuery()
-        con.Close()
-
-        ShowTotals()
-    End Sub
-
-    Private Sub btnTpaid_Click(sender As Object, e As EventArgs)
-        cmd.Parameters.Clear()
-        cmd.CommandText = "INSERT INTO Tickets (RegistrationDate, TicketType, Total, UserID) VALUES (@RD, 3, 110, @UID)"
-        cmd.Parameters.Add(New SqlParameter("@RD", DateTime.Now))
-        cmd.Parameters.Add(New SqlParameter("@UID", Session.UserID))
-
-        con.Open()
-        cmd.ExecuteNonQuery()
-        con.Close()
-
-        ShowTotals()
-    End Sub
-
-    Private Sub btnBack_Click_1(sender As Object, e As EventArgs) Handles btnBack.Click
-        Close()
-    End Sub
 
     Private Sub btnCpaid_Click_1(sender As Object, e As EventArgs) Handles btnCpaid.Click
         cmd.Parameters.Clear()
@@ -106,5 +73,9 @@ Public Class frmFirstShift
         con.Close()
 
         ShowTotals()
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Close()
     End Sub
 End Class
